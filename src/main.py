7ceb3de323b527a33e7bcb08e352fcef08b64e20
Tiffany Chen <yuhan210@gmail.com>
2015-02-06 22:41:15 -0600

    start cracking main

--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ from Utility import *
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) != 2):
+    if ( len(sys.argv) != 2 ):
         print "Usage:", sys.argv[0], " client_num" 
         exit(-1)
 
+    # create a list of clients
     u = Utility("")
     u.get_utility(8)
+    
+    sim_duration = 200000
+    # each step, client with pending jobs determines whether to send a job with some nwk delay   
+    while     
+   
+    #    
