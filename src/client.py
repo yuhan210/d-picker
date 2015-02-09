2e8aa8490030f71a7df578244543a1ca3a7c73ab
Tiffany Chen <yuhan210@gmail.com>
2015-02-09 13:50:10 -0800

    exp dist implemented and tested

--- src/client.py
+++ src/client.py
@@ -1,12 +1,25 @@
+import math
 from Utility import *
+from random import *
 
 class Client(object):
-    nwk_delay = -1
+    nwk_delay = -1.0
     utility_fn = None
-
-    def __init__(self, utility_fn_name, para, nwk_delay):
+    utility_fn_name = ""
+    utility_para = -1.0
+    job_gen_para = -1.0
+    job_start_time = -1  
+    def __init__(self, utility_fn_name, utility_para, job_para,nwk_delay):
         
         self.nwk_delay = nwk_delay
-        self.utility_fn = Utility(utility_fn_name, para)
-
-
+        self.utility_fn_name = utility_fn_name
+        self.utility_para = utility_para
+        self.job_gen_para = job_para
+        self.utility_fn = Utility(self.utility_fn_name, self.utility_para)
+        self.job_start_time = self.exponential(self.job_gen_para)
+    
+    def exponential(self, para): # para -> lambda
+        return (-1.0)* para * math.log(1 - random()) 
+    
+    def getStartTime(self):
+        return self.job_start_time
