24e1f3c97b5967fd76562226faa651be871e16ce
Tiffany Chen <yuhan210@gmail.com>
2015-02-10 14:37:35 -0800

    FIFO scheduler implemented

--- src/FIFOScheduler.py
+++ src/FIFOScheduler.py
@@ -1,3 +1,16 @@
-class FIFOScheduler(object):
+from Utility import *
+from Client import *
 
-    def __init__(self):
+class FIFO(object):
+
+#    def __init__(self):
+    def schedule(self, cur_time, jobs):
+        if len(jobs) == 0:
+            return 0.0
+        score = 0.0
+
+        oldest_job = min(filter(lambda x: x.getJobArrivalTime(), jobs))
+        score += oldest_job.getServed(cur_time)
+        
+        print "Time:", cur_time, "serve job:", oldest_job.getCid(), "receiving utility:", score
+        return score        
